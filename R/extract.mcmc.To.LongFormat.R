97c2fc6149819c634503ece0d75a6cc32736674a
Marco Kienzle <Marco.Kienzle@gmail.com>
2019-07-31 16:46:40 +1200

    Added functionality to plot trends in SSB from MCMC

---
+++ R/extract.mcmc.To.LongFormat.R
@@ -0,0 +1,19 @@
+# A function that reads the output of a CASAL MCMC (a wide format table) and convert it into a long format with 2 columns
+# key: contains the value of the parameter, quantity, etc...
+# value:: contains the value of the parameter, quantity, etc...
+extract.mcmc.To.LongFormat = function(mcmcfilename, path){
+
+  # Load the MCMC file
+  library(casal)
+  mcmc.InputValues = extract.mcmc(samples =  mcmcfilename, path = path)
+
+  # Reset the class of the object to matrix to allow manipulation
+  class(mcmc.InputValues) <- "matrix"
+
+  # Convert data input from wide to long format
+  library(tidyr)
+  mcmc.InputValues.long = gather(data = as.data.frame(mcmc.InputValues))
+
+  return(mcmc.InputValues.long)
+}
+
---
+++ R/plot.MCMC.SSB.R
@@ -0,0 +1,54 @@
+plot.MCMC.SSB <- function(mcmcfilename, path){
+
+  # Get all the MCMC the data
+  library(tidyverse)
+  mcmc.data = as_tibble(extract.mcmc.To.LongFormat(mcmcfilename = mcmcfilename, path = path))
+
+  # Subset the data for string that look like SSB[2017]
+  mcmc.SSB.data = mcmc.data %>% filter(grepl("SSB\\[[0-9]{4}\\]", key))
+
+  # Extract year from the label(new.df = mcmc.SSB.data %>% dplyr::mutate(year =  as.numeric(str_extract(key, "[0-9]{4}"))))
+  SSB.data = mcmc.SSB.data %>% dplyr::mutate(year =  as.numeric(str_extract(key, "[0-9]{4}")))
+
+  # A function to calculate the quantiles of the distribution
+  mean_cl_quantile <- function(x, q = c(0.025, 0.975), na.rm = TRUE){
+    dat <- data.frame(y = mean(x, na.rm = na.rm),
+                      ymin = quantile(x, probs = q[1], na.rm = na.rm),
+                      ymax = quantile(x, probs = q[2], na.rm = na.rm))
+    return(dat)
+  }
+
+
+  # Plot
+  my.p = ggplot(data = SSB.data, aes(x = year, y = value)) +
+    stat_summary(geom = "line", fun.y = median, col = "lightgrey", size = 1.2) +
+    stat_summary(geom = "ribbon", fun.data = mean_cl_quantile, alpha = 0.3) +
+    xlab("") + ylab("SSB (t)") +
+    theme_light()
+
+  #plot(my.p)
+
+  # Add managment reference point
+  if(mgt.ref.points){
+
+    # Calculate virgin biomass
+    median.B0 = as.numeric(mcmc.data %>% filter(grepl("B0", key)) %>% summarize(median(value)))
+
+    # location of the labels on the x-axis
+    lab.x.axis = 1980
+
+    # create a data.frame with label info
+    lbl.df = data.frame(x = lab.x.axis, y = c(0.1, 0.2, 0.4, 1) * median.B0, lbl = c("10% B0", "20% B0", "40% B0", "100% B0"))
+
+    # Add lines to plot
+    my.p <- my.p +
+      geom_hline(aes(yintercept = y), data = lbl.df, lwd = 1, lty = 2, col = "black", show.legend = FALSE) +
+      geom_label( mapping = aes(x = x, y = y, label = lbl), data = lbl.df, fill = "white")
+
+  }
+
+  return(my.p)
+
+
+
+}
