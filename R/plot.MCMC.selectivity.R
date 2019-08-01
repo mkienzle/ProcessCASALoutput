2e3cb450ea99d22116647418c85047d79c2c618d
Marco Kienzle <Marco.Kienzle@gmail.com>
2019-08-01 16:12:59 +1200

    providing a solution to extract age from CASAL output files in plot.MCMC.selectivity

--- R/plot.MCMC.selectivity.R
+++ R/plot.MCMC.selectivity.R
@@ -8,11 +8,12 @@ plot.MCMC.selectivity <- function(mcmcfilename, path, selectivity.label){
   library(tidyverse)
   mcmc.data = as_tibble(extract.mcmc.To.LongFormat(mcmcfilename = mcmcfilename, path = path))
 
-  # Subset the data for string that look like "SSB" with year in square brackets, for example SSB[1997]
+  # extract age
   mcmc.selectivity.data = mcmc.data %>% filter(grepl(paste("selectivity\\[", selectivity.label, "\\]", sep=""), key))
 
   # Extract year from the label(new.df = mcmc.SSB.data %>% dplyr::mutate(year =  as.numeric(str_extract(key, "[0-9]{4}"))))
-  selectivity.data = mcmc.selectivity.data %>% dplyr::mutate(age =  as.numeric(str_extract(key, "[[:digit:]]+")))
+  selectivity.data = mcmc.selectivity.data %>% dplyr::mutate(tmp =  str_extract(key, "\\[[0-9]{1,2}\\]"))  %>% dplyr::mutate(age =  as.numeric(str_extract(tmp, "[0-9]{1,2}")))
+
 
   # A function to calculate the quantiles of the distribution
   mean_cl_quantile <- function(x, q = c(0.025, 0.975), na.rm = TRUE){
@@ -23,12 +24,12 @@ plot.MCMC.selectivity <- function(mcmcfilename, path, selectivity.label){
   }
 
   # Plot
-  (my.p = ggplot(data = selectivity.data, aes(x = age, y = value)) +
+  my.p = ggplot(data = selectivity.data, aes(x = age, y = value)) +
     stat_summary(geom = "line", fun.y = median, col = "black", size = 1.2) +
     stat_summary(geom = "ribbon", fun.data = mean_cl_quantile, alpha = 0.1, lty = 3) +
     xlab("age-group") + ylab("Proportion retained") +
       labs(caption = selectivity.label) +
-    theme_light())
+    theme_light()
 
 
   return(my.p)
