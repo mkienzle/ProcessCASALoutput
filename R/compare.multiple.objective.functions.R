25e518468e95d13f0969d97f031920bb70b6c7e7
Marco Kienzle <Marco.Kienzle@gmail.com>
2020-02-07 16:45:26 +1300

    would be good to have the total log-likelihood

--- R/compare.multiple.objective.functions.R
+++ R/compare.multiple.objective.functions.R
@@ -10,6 +10,7 @@ compare.multiple.objective.functions <- function(filenames, models.names){
     if(file.nb == 1) {
                 tmp.data = extract.multiple.objective.functions(filenames[file.nb])
                 my.df = data.frame( loglik.comp = dimnames(tmp.data)[[1]], value = tmp.data, model.name = models.names[file.nb])
+                #my.df = rbind(my.df, data.frame())
                 } else {
                 tmp.data = extract.multiple.objective.functions(filenames[file.nb])
                 my.df = rbind(my.df, data.frame( loglik.comp = dimnames(tmp.data)[[1]], value = tmp.data, model.name = models.names[file.nb]))}
@@ -20,15 +21,16 @@ compare.multiple.objective.functions <- function(filenames, models.names){
 
   fct.result = as_tibble(my.df) %>% spread(model.name, value)
   fct.result %>% print(n=100)
+  print("Total"); dummy %>% summarise_if(is.numeric, funs(sum)) %>% print(n=10)
 
   return(fct.result)
 }
 
 # Example
 
-#dummy = compare.multiple.objective.functions(
-#  filenames = c("C:/Users/kienzlemj/OneDrive - NIWA/Projects/Stock assessments/Ling/LIN 7/LIN201903/CASAL/LIN7_comb_v44/CASAL-MPDoutput.txt",
-#                "C:/Users/kienzlemj/OneDrive - NIWA/Projects/Stock assessments/Ling/LIN 7/LIN201903/CASAL/LIN7_logn_v44/CASAL-MPDoutput.txt"),
-#  models.names = c("Model A", "Model B")
-#  )
+# dummy = compare.multiple.objective.functions(
+#   filenames = c("C:/Users/kienzlemj/OneDrive - NIWA/Projects/Stock assessments/Ling/LIN 7/LIN201903/CASAL/LIN7_comb_v44/CASAL-MPDoutput.txt",
+#                 "C:/Users/kienzlemj/OneDrive - NIWA/Projects/Stock assessments/Ling/LIN 7/LIN201903/CASAL/LIN7_logn_v44/CASAL-MPDoutput.txt"),
+#   models.names = c("Model A", "Model B")
+#   )
 
