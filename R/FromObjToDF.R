f09cdb30aeefce574f016091ecb2f8eea6172560
Marco Kienzle <Marco.Kienzle@gmail.com>
2020-04-06 09:59:38 +1000

    changed FromObjToDF to accommodate dataset by age as well as dataset without age dimension

--- R/FromObjToDF.R
+++ R/FromObjToDF.R
@@ -4,20 +4,29 @@
 
 FromObjToDF <- function(obj){
 
-  # Get
-  year <- obj$year
 
-  # Create a matrix of age-groups corresponding to each observations, remove leading X
-  age.group <- as.numeric(substr(dimnames(obj$obs)[[2]], 2, 10))
-  age.groups <- outer(rep(1, nrow(obj$obs)), age.group)
+  # Here we assume that if the obs in the obj is a data.frame, it is a table of year x age
+  if( is.data.frame(obj$obs)){
+    year <- obj$year
+    age.group <- as.numeric(substr(dimnames(obj$obs)[[2]], 2,
+                                   10))
+    age.groups <- outer(rep(1, nrow(obj$obs)), age.group)
+    years <- outer(year, rep(1, ncol(obj$obs)))
+    df <- data.frame(year = c(years), age.group = c(age.groups),
+                     observations = c(as.matrix(obj$obs)), predictions = c(as.matrix(obj$fits)),
+                     pearson.residuals = c(as.matrix(obj$pearson.resids)))
 
-  # Create a matrix of years corresponding to every observations
-  years <- outer(year, rep(1, ncol(obj$obs)))
+  } # End of the case where it's a data.frame
 
-  # Format into a data.frame
-  df <- data.frame(year = c(years),
-                   age.group = c(age.groups),
-                   observations = c(as.matrix(obj$obs)),
-                   predictions = c(as.matrix(obj$fits)),
-                   pearson.residuals = c(as.matrix(obj$pearson.resids)))
+  # Otherwise we assume it's a vector of observations per year (no aged)
+  else {
+    age.groups <- NA
+    years <- obj$year
+    df <- data.frame(year = c(years), age.group = c(age.groups),
+                     observations = c(as.matrix(obj$obs)), predictions = c(as.matrix(obj$fits)),
+                     pearson.residuals = c(as.matrix(obj$pearson.resids)))
+
+  } # End of the case where it's NOT a data.frame
+
+
 }
